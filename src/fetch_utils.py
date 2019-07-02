d2e1f59d605756da16b57e697e408a9cb1b6ac8d
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-02 16:55:24 +0200

    Adding tests and functions to retrieve tarballs from FreeBSD's repos

---
+++ src/fetch_utils.py
@@ -0,0 +1,34 @@
+from ftplib import FTP
+from pathlib import PosixPath
+
+from models.jail import Jail
+from models.version import VersionType, Version
+
+SERVER_URL = "ftp.FreeBSD.org"
+FTP_BASE_DIRECTORY = PosixPath('pub/FreeBSD')
+
+
+def fetch_tarballs_into(jail_info: Jail, temp_dir: PosixPath):
+    ftp_connector = FTP(SERVER_URL)
+    ftp_connector.login()
+
+    directory = get_directory_path(jail_info.architecture, jail_info.version)
+    ftp_connector.cwd(directory.as_posix())
+
+    components = jail_info.components.copy()
+    components.append('base')
+
+    for component in components:
+        temp_file = temp_dir.joinpath(f"{component}.txz").as_posix()
+        ftp_connector.retrbinary(f"RETR {component}.txz", open(temp_file, 'wb').write)
+
+    ftp_connector.quit()
+
+
+def get_directory_path(architecture: str, version: Version):
+    if version.version_type == VersionType.RELEASE:
+        directory = FTP_BASE_DIRECTORY.joinpath('releases')
+    else:
+        directory = FTP_BASE_DIRECTORY.joinpath('snapshots')
+    directory = directory.joinpath(architecture, str(version))
+    return directory
---
+++ src/test/fetch_utils_pytest.py
@@ -0,0 +1,17 @@
+from pathlib import PosixPath
+from tempfile import TemporaryDirectory
+
+from models.jail import Jail
+from models.version import Version, VersionType
+from src.fetch_utils import fetch_tarballs_into
+
+
+class TestFetchUtils:
+    def test_fetch_base_tarball(self):
+        jail_version = Version(major=12, minor=0, version_type=VersionType.RELEASE)
+        jail = Jail(name='name', version=jail_version, architecture='amd64', components=[])
+
+        with TemporaryDirectory() as temp_dir:
+            temp_dir_path = PosixPath(temp_dir)
+            fetch_tarballs_into(jail, temp_dir_path)
+            assert temp_dir_path.joinpath('base.txz').is_file()
