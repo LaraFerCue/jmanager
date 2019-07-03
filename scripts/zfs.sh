f1b5687cbd3b0ae15f717f5f7c2a91b7becee86d
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-03 15:00:57 +0200

    Adding syntax checks for ZFS create following documentation in zfs(8)

--- scripts/zfs.sh
+++ scripts/zfs.sh
@@ -1,8 +1,44 @@
 #!/bin/sh
 
+check_create()
+{
+	while [ "${1}" ] ; do
+		case "${1}" in
+			-p|-u)
+				;;
+			-o)
+				if echo "${2}" | grep -qvE '^[^=]+=.+$' ; then
+					return 1
+				fi
+				shift
+				;;
+			-V)
+				if echo "${2}" | grep -qvE '^[0-9]+[kKmMgG]*$' ; then
+					return 1
+				fi
+				shift
+				;;
+			*)
+				break
+				;;
+		esac
+		shift
+	done
+
+	if echo "${1}" | grep -qE '^-' ; then
+		return 1
+	fi
+	return 0
+}
+
 case "${1}" in
 	error)
 		exit 1
+       ;;
+	create)
+		shift
+		check_create "${@}"
+		exit "${?}"
 		;;
 	*)
 		exit 0
