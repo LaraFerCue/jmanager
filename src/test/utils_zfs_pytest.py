631b1e1b964e6569e661a3864ab2f236e897a10e
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-03 17:18:14 +0200

    Changing all tests to use the created dataset

--- src/test/utils_zfs_pytest.py
+++ src/test/utils_zfs_pytest.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.utils.zfs import zfs_cmd, ZFSError, zfs_list, ZFSProperty
 
-TEST_DATA_SET = 'zroot/jmanager_test/test'
+TEST_DATA_SET = 'zroot/jmanager_test'
 
 
 class TestZFS:
@@ -15,16 +15,16 @@ class TestZFS:
             zfs_cmd(cmd='error', arguments=[], options={}, data_set='zroot')
 
     def test_zfs_cmd_with_options(self):
-        zfs_cmd(cmd="create", arguments=["-u"], options={'canmount': 'on'}, data_set=TEST_DATA_SET)
-        zfs_cmd(cmd="destroy", arguments=[], options={}, data_set=TEST_DATA_SET)
+        zfs_cmd(cmd="create", arguments=["-u"], options={'canmount': 'on'}, data_set=f"{TEST_DATA_SET}/test")
+        zfs_cmd(cmd="destroy", arguments=[], options={}, data_set=f"{TEST_DATA_SET}/test")
 
     def test_zfs_list_without_options(self):
-        output = zfs_list(data_set='zroot')
+        output = zfs_list(data_set=TEST_DATA_SET)
 
         for data_set in output:
-            assert ZFSProperty.NAME in data_set and data_set[ZFSProperty.NAME] == 'zroot'
+            assert ZFSProperty.NAME in data_set and data_set[ZFSProperty.NAME] == TEST_DATA_SET
             assert ZFSProperty.USED in data_set and ZFSProperty.AVAIL in data_set and ZFSProperty.REFER in data_set
-            assert ZFSProperty.MOUNTPOINT in data_set and data_set[ZFSProperty.MOUNTPOINT] == '/zroot'
+            assert ZFSProperty.MOUNTPOINT in data_set and data_set[ZFSProperty.MOUNTPOINT] == f"/{TEST_DATA_SET}"
 
     def test_zfs_list_depth_option(self):
         output = zfs_list(data_set='zroot', depth=1)
