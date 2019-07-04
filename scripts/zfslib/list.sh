d2f12e15850ea956435d052fb78956eb1bd288bf
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-04 11:52:55 +0200

    Adding filtering options for the emulated ZFS list command

--- scripts/zfslib/list.sh
+++ scripts/zfslib/list.sh
@@ -8,21 +8,84 @@ ZFSLIB_PATH="$(realpath "$(dirname "${0}")")"
 # shellcheck source=zfslib/common.sh
 . "${ZFSLIB_PATH}/common.sh"
 
+ZFS_TYPES="filesystem|snapshot|snap|volume|bookmark|all"
+get_entries()
+{
+	local dataset=${1:-""}
+	local depth=${2:-0}
+
+	if [ "${depth}" -eq -2 ] ; then
+		grep -E "^${dataset}" "${ZFS_TEST_DATABASE}"
+	elif [ "${depth}" -eq -1 ] ; then
+		return 0
+	else
+		grep -E "^${dataset};" "${ZFS_TEST_DATABASE}"
+		get_entries "${dataset}/[^/]+" "$((depth - 1))"
+	fi
+}
+
 show_entries()
 {
+	local columns=${1:-1,2,3,4,5}
 	local entry
 
 	while read -r entry ; do
 		echo "${entry}" | tr ';' '\t'
-	done
+	done | cut -w -f "${columns}"
 }
 
-regex=""
-zfs_type="filesystem|volume|clone"
+filter_entries()
+{
+	local types=${1}
+
+	grep -E ";(${types});"
+}
+
+get_options()
+{
+	local options option_index
+
+	options=""
+	for option in $(echo "${OPTARG}" | tr ',' ' '); do
+		option_index=0
+		case "${option}" in
+			name)
+				option_index=1
+				;;
+			used)
+				option_index=2
+				;;
+			avail)
+				option_index=3
+				;;
+			refer)
+				option_index=4
+				;;
+			mountpoint)
+				option_index=5
+				;;
+			type)
+				option_index=6
+				;;
+		esac
+		[ "${option_index}" -eq 0 ] && continue
+		if [ "${options}" ] ; then
+			options="${options},${option_index}"
+		else
+			options="${option_index}"
+		fi
+	done
+
+	echo "${options}"
+}
+
+depth=0
+columns="1,2,3,4,5"
+types=".*"
 while getopts "rd:Hpo:t:s:S:" "args" ; do
 	case "${args}" in
 		r)
-			regex="^%name%.*;(%type%);"
+			depth=-2
 			;;
 		H|p)
 			;;
@@ -30,17 +93,14 @@ while getopts "rd:Hpo:t:s:S:" "args" ; do
 			check_value "^[0-9]+$" || return 1
 
 			depth="${OPTARG}"
-			regex="^%name%"
-			while [ "${depth}" -gt 0 ] ; do
-				regex="${regex}/?[^/]*"
-				: $((depth -= 1))
-			done
 			;;
 		o)
 			check_value "^[^,]+(,[^,]+)*$" || return 1
+			columns="$(get_options)"
 			;;
 		t)
-			check_value "^[^,]+(,[^,]+)*$" || return 1
+			check_value "^(${ZFS_TYPES}),?(${ZFS_TYPES})*$" || return 1
+			types=$(echo "${OPTARG}" | sed 's/,/|/g')
 			;;
 		s|S)
 			check_value "^[a-zA-Z]+$" || return 1
@@ -53,7 +113,7 @@ done
 dataset_name="$(eval echo "\${${OPTIND}}")"
 check_dataset_name "${dataset_name}"
 
-regex=$(replace_wildcards "${regex}" "${dataset_name}" "${zfs_type}")
-grep -E "${regex}" "${ZFS_TEST_DATABASE}" | show_entries
+get_entries "${dataset_name}" "${depth}" | filter_entries "${types}" | \
+	show_entries "${columns}"
 
 
