bd1ebe5ce94037cc9d3498909a8654d7816bc509
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-04 17:59:45 +0200

    Creation of the base jail by extracting the components into it

--- src/factories/jail_factory.py
+++ src/factories/jail_factory.py
@@ -1,4 +1,5 @@
 from pathlib import PosixPath
+from tarfile import TarFile
 
 from models.distribution import Distribution
 from models.jail import Jail
@@ -8,13 +9,29 @@ from src.utils.zfs import ZFS
 class JailFactory:
     ZFS_FACTORY = ZFS()
 
-    def __init__(self, jail_root_path: PosixPath):
+    def __init__(self, jail_root_path: PosixPath, zfs_root_data_set: str):
         self._jail_root_path = jail_root_path
+        self._zfs_root_data_set = zfs_root_data_set
 
     def create_base_jail(self, distribution: Distribution, path_to_tarballs: PosixPath):
         for component in distribution.components:
             if not path_to_tarballs.joinpath(f"{component.value}.txz").is_file():
                 raise FileNotFoundError(f"Component '{component.value}' not found in {path_to_tarballs}")
+
+        jail_path = f"{distribution.version}_{distribution.architecture.value}"
+        self.ZFS_FACTORY.zfs_create(
+            data_set=f"{self._zfs_root_data_set}/{jail_path}",
+            options={"mountpoint": f"{self._jail_root_path}/{jail_path}"}
+        )
+
+        for component in distribution.components:
+            with TarFile(path_to_tarballs.joinpath(f"{component.value}.txz").as_posix()) as tarfile:
+                tarfile.extractall(path=f"{self._jail_root_path}/{jail_path}")
+
+    def exists_jail(self, distribution: Distribution):
+        list_of_datasets = self.ZFS_FACTORY.zfs_list(
+            f"{self._zfs_root_data_set}/{distribution.version}_{distribution.architecture.value}")
+        return len(list_of_datasets) > 0
 
     def create_jail(self, jail_data: Jail, distribution: Distribution, path_to_tarballs: PosixPath):
         pass
--- src/test/utils_jail_pytest.py
+++ src/test/utils_jail_pytest.py
@@ -6,7 +6,7 @@ import pytest
 
 from models.distribution import Distribution, Version, VersionType, Architecture
 from src.factories.jail_factory import JailFactory
-from src.test.globals import MockingZFS
+from src.test.globals import MockingZFS, TEST_DATA_SET
 
 TMP_PATH = PosixPath('/tmp')
 
@@ -22,7 +22,7 @@ def create_dummy_tarball_in_folder(path_to_folder: PosixPath):
 
 class TestUtilsJail:
     def test_create_base_data_set_without_tarballs(self):
-        jail_factory = MockingJailFactory(TMP_PATH)
+        jail_factory = MockingJailFactory(TMP_PATH, TEST_DATA_SET)
         distribution = Distribution(version=Version(12, 0, VersionType.RELEASE), architecture=Architecture.AMD64,
                                     components=[])
 
@@ -31,13 +31,17 @@ class TestUtilsJail:
                 jail_factory.create_base_jail(distribution, PosixPath(temp_dir))
 
     def test_create_base_data_set(self):
-        jail_factory = MockingJailFactory(TMP_PATH)
         distribution = Distribution(version=Version(12, 0, VersionType.RELEASE), architecture=Architecture.AMD64,
                                     components=[])
 
         with TemporaryDirectory() as temp_dir:
+            jail_factory = MockingJailFactory(temp_dir, TEST_DATA_SET)
             create_dummy_tarball_in_folder(PosixPath(temp_dir))
             try:
                 jail_factory.create_base_jail(distribution=distribution, path_to_tarballs=PosixPath(temp_dir))
+                assert jail_factory.exists_jail(distribution=distribution)
+                assert PosixPath(temp_dir).joinpath(
+                    f"{distribution.version}_{distribution.architecture.value}").iterdir()
             finally:
-                pass
+                jail_factory.ZFS_FACTORY.zfs_destroy(
+                    f"{TEST_DATA_SET}/{distribution.version}_{distribution.architecture.value}")
