001b31f83090093dfb24adaac27da154061e3e03
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-05 12:16:09 +0200

    Add clone command to the zfs emulator

---
+++ scripts/zfslib/clone.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# shellcheck disable=SC2039
+# SC2039: local is defined in Bourne Shell and Bash
+
+: "${ZFSLIB_PATH:?Error: ZFSLIB_PATH not set}"
+
+# shellcheck source=zfslib/common.sh
+. "${ZFSLIB_PATH}/common.sh"
+
+set -x
+create_parents=false
+options="origin=%name%"
+while getopts "po:" "args" ; do
+	case "${args}" in
+		p)
+			create_parents=true
+			;;
+		o)
+			if ! check_value '^[^=]+=.+$' ; then
+				return 1
+			fi
+			options="${options},${OPTARG}"
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
+dataset_name="$(eval echo "\${${OPTIND}}")"
+snapshot_name=$(echo "${dataset_name}" | awk -F '@' '{ print $2 }')
+dataset_name=${dataset_name%%@${snapshot_name}}
+
+shift
+clone_name="$(eval echo "\${${OPTIND}}")"
+check_dataset_name "${dataset_name}" || exit 1
+
+grep -qE "^${clone_name};" "${ZFS_TEST_DATABASE}" && return 1
+grep -qE "^${dataset_name}@${snapshot_name};" "${ZFS_TEST_DATABASE}" || return 1
+if "${create_parents}" ; then
+	create_recursive_datasets "${clone_name}" "clone" "${options}"
+elif grep -qE "^$(get_parent_dataset "${clone_name}")" "${ZFS_TEST_DATABASE}" ; then
+	create_dataset "${clone_name}" "clone" "${options}"
+else
+	exit 1
+fi
