265bc4297d1d0ce355fdb2bbd42140dec2cced0c
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-08 18:10:27 +0200

    Move the command parsing to a new file to improve readability.

--- jmanager/__main__.py
+++ jmanager/__main__.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-from pathlib import PosixPath
-from typing import Dict
 
-from jmanager.jail_manager import JailManager
-from src.configuration import read_configuration_file, parse_jmanagerfile
-from src.factories.jail_factory import JailFactory
-from src.utils.fetch import HTTPFetcher
+from jmanager.commands import execute_commands
 
 parser = argparse.ArgumentParser(description="FreeBSD Jail Manager")
 parser.add_argument("--jmanager-config", type=str,
@@ -27,23 +22,4 @@ destroy_parser.set_defaults(command='destroy')
 destroy_parser.add_argument("jail_name", type=str, help="name of the jail to destroy")
 
 args = parser.parse_args()
-
-config_file_path = PosixPath(args.jmanager_config)
-configuration: Dict[str, str] = read_configuration_file(config_file_path)
-jail_factory = JailFactory(
-    jail_root_path=PosixPath(configuration['jail_base_path']),
-    zfs_root_data_set=configuration['zfs_root_dataset'],
-    jail_config_folder=PosixPath(configuration['jmanager_config_dir'])
-)
-
-jail_manager = JailManager(http_fetcher=HTTPFetcher(),
-                           jail_factory=jail_factory)
-
-if args.command == 'create':
-    jmanagerfile_path = PosixPath(args.jmanagerfile)
-    jmanagerfile_list = parse_jmanagerfile(read_configuration_file(jmanagerfile_path))
-
-    for jmanagerfile in jmanagerfile_list:
-        jail_manager.create_jail(jail_data=jmanagerfile.jail, distribution=jmanagerfile.distribution)
-elif args.command == 'destroy':
-    jail_manager.destroy_jail(args.jail_name)
+execute_commands(args)
---
+++ jmanager/commands.py
@@ -0,0 +1,34 @@
+from argparse import Namespace
+from pathlib import PosixPath
+from typing import Dict
+
+from jmanager.jail_manager import JailManager
+from src.configuration import read_configuration_file, parse_jmanagerfile
+from src.factories.jail_factory import JailFactory
+from src.utils.fetch import HTTPFetcher
+
+
+def create_command(jmanagerfile: str, jail_manager: JailManager):
+    jmanagerfile_path = PosixPath(jmanagerfile)
+    jmanagerfile_list = parse_jmanagerfile(read_configuration_file(jmanagerfile_path))
+
+    for jmanagerfile in jmanagerfile_list:
+        jail_manager.create_jail(jail_data=jmanagerfile.jail, distribution=jmanagerfile.distribution)
+
+
+def execute_commands(args: Namespace):
+    config_file_path = PosixPath(args.jmanager_config)
+    configuration: Dict[str, str] = read_configuration_file(config_file_path)
+    jail_factory = JailFactory(
+        jail_root_path=PosixPath(configuration['jail_base_path']),
+        zfs_root_data_set=configuration['zfs_root_dataset'],
+        jail_config_folder=PosixPath(configuration['jmanager_config_dir'])
+    )
+
+    jail_manager = JailManager(http_fetcher=HTTPFetcher(),
+                               jail_factory=jail_factory)
+
+    if args.command == 'create':
+        create_command(jail_manager=jail_manager, jmanagerfile=args.jmanagerfile)
+    elif args.command == 'destroy':
+        jail_manager.destroy_jail(args.jail_name)
