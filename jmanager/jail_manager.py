dfbceffcc35b10ce728c12063a7355c09c59868e
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-08 17:11:29 +0200

    Create a jail manager class to deal with the commands to create a jail from scratch

---
+++ jmanager/jail_manager.py
@@ -0,0 +1,37 @@
+from pathlib import PosixPath
+from tempfile import TemporaryDirectory
+
+from models.distribution import Distribution
+from models.jail import Jail
+from src.factories.jail_factory import JailFactory
+from src.utils.fetch import HTTPFetcher
+
+
+class JailManager:
+    def __init__(self, http_fetcher: HTTPFetcher, jail_factory: JailFactory):
+        self._http_fetcher = http_fetcher
+        self._jail_factory = jail_factory
+
+    @staticmethod
+    def print_progress_bar(msg, iteration, total):
+        percent = "{0:.1f}".format(100 * (iteration / float(total)))
+        filled_length = int(50 * iteration // total)
+        bar = '=' * filled_length + ' ' * (50 - filled_length)
+        print('\r%s |%s| %s%%' % (msg, bar, percent), end='\r')
+        # Print New Line on Complete
+        if iteration == total:
+            print()
+
+    def create_jail(self, jail_data: Jail, distribution: Distribution):
+        if not self._jail_factory.base_jail_exists(distribution=distribution):
+            if self._jail_factory.base_jail_incomplete(distribution=distribution):
+                self._jail_factory.destroy_base_jail(distribution=distribution)
+
+            with TemporaryDirectory(prefix="jmanager_", suffix="_tarballs") as temp_dir:
+                path_to_temp_dir = PosixPath(temp_dir)
+                self._http_fetcher.fetch_tarballs_into(distribution=distribution, temp_dir=path_to_temp_dir,
+                                                       callback=JailManager.print_progress_bar)
+                self._jail_factory.create_base_jail(distribution=distribution, path_to_tarballs=path_to_temp_dir)
+
+        self._jail_factory.create_jail(jail_data=jail_data, os_version=distribution.version,
+                                       architecture=distribution.architecture)
