dd32866a43ed69d85cfa0cd4c96ecf13517e3e73
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-10 16:26:24 +0200

    Create test for accumulative destruction of base jails

--- src/test/base_jail_factory_pytest.py
+++ src/test/base_jail_factory_pytest.py
@@ -100,6 +100,33 @@ class TestBaseJailFactory:
         assert not base_jail_factory.base_jail_incomplete(TEST_DISTRIBUTION)
         assert not base_jail_factory.base_jail_exists(distribution=TEST_DISTRIBUTION)
 
+    def test_destroy_base_jail_with_accumulative_components(self):
+        distribution = Distribution(version=TEST_DISTRIBUTION.version,
+                                    architecture=TEST_DISTRIBUTION.architecture,
+                                    components=[Component.SRC, Component.LIB32])
+        lib32_distribution = Distribution(version=TEST_DISTRIBUTION.version,
+                                          architecture=TEST_DISTRIBUTION.architecture,
+                                          components=[Component.LIB32])
+        base_jail_factory = get_mocking_base_jail_factory(TMP_PATH)
+        create_dummy_base_jail()
+        create_dummy_base_jail(distribution=distribution)
+        create_dummy_base_jail(distribution=lib32_distribution)
+
+        base_jail_factory.destroy_base_jail(distribution)
+        assert base_jail_factory.base_jail_exists(distribution=TEST_DISTRIBUTION)
+        assert base_jail_factory.base_jail_exists(distribution=lib32_distribution)
+        assert not base_jail_factory.base_jail_exists(distribution=distribution)
+
+        base_jail_factory.destroy_base_jail(lib32_distribution)
+        assert base_jail_factory.base_jail_exists(distribution=TEST_DISTRIBUTION)
+        assert not base_jail_factory.base_jail_exists(distribution=lib32_distribution)
+        assert not base_jail_factory.base_jail_exists(distribution=distribution)
+
+        base_jail_factory.destroy_base_jail(TEST_DISTRIBUTION)
+        assert not base_jail_factory.base_jail_exists(distribution=TEST_DISTRIBUTION)
+        assert not base_jail_factory.base_jail_exists(distribution=lib32_distribution)
+        assert not base_jail_factory.base_jail_exists(distribution=distribution)
+
     def test_create_base_data_set_without_tarballs(self):
         base_jail_factory = get_mocking_base_jail_factory(TMP_PATH)
         with TemporaryDirectory() as temp_dir:
