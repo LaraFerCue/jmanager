fa475fcd1cd79809f847fd1cf454315e9e99b1a0
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-12 16:24:08 +0200

    Add functions to write ansible configuration and inventory

--- src/test/utils_provision_pytest.py
+++ src/test/utils_provision_pytest.py
@@ -5,6 +5,8 @@ from tempfile import TemporaryDirectory
 
 import pytest
 
+from models.jail import Jail
+from src.test.globals import TMP_PATH
 from src.utils.provision import Provision
 
 TEST_RESOURCES_PLAYBOOK = PosixPath('src/test/resources/playbook')
@@ -12,6 +14,12 @@ TEST_RESOURCES_PLAYBOOK = PosixPath('src/test/resources/playbook')
 
 class MockingProvision(Provision):
     ANSIBLE_CMD = "sh scripts/ansible.sh"
+
+
+def configure_ansible_template(temp_folder_path: str, filename: str):
+    with open('src/test/resources/ansible.cfg', 'r') as template:
+        with open(f"{temp_folder_path}/{filename}", 'w') as temp_file:
+            temp_file.write(template.read().replace('%temp%', temp_folder_path))
 
 
 class TestProvision:
@@ -35,3 +43,13 @@ class TestProvision:
             MockingProvision().run_provision(
                 path_to_playbook_file=TEST_RESOURCES_PLAYBOOK.joinpath('none'),
                 wrap_output=True)
+
+    def test_create_ansible_configuration(self):
+        with TemporaryDirectory() as temp_dir:
+            MockingProvision().write_ansible_configuration(PosixPath(temp_dir))
+            configure_ansible_template(temp_dir, 'config')
+            assert filecmp.cmp(f"{temp_dir}/ansible.cfg", f"{temp_dir}/config", shallow=False)
+
+    def test_write_inventory_file(self):
+        MockingProvision().write_inventory([Jail('test1'), Jail('test2')], TMP_PATH)
+        assert filecmp.cmp(TMP_PATH.joinpath('ansible_inventory').as_posix(), 'src/test/resources/ansible_inventory')
--- src/utils/provision.py
+++ src/utils/provision.py
@@ -1,11 +1,16 @@
 import subprocess
 from pathlib import PosixPath
+from typing import List, Dict
 
 import yaml
+
+from models.jail import Jail
 
 
 class Provision:
     ANSIBLE_CMD = 'ansible-playbook-3.6'
+    ANSIBLE_INVENTORY_NAME = 'ansible_inventory'
+    ANSIBLE_CONFIG_FILE = 'ansible.cfg'
 
     @staticmethod
     def write_ansible_playbook(path_to_file: PosixPath, ansible_playbook):
@@ -25,3 +30,30 @@ class Provision:
             subprocess_options['stdout'] = subprocess.PIPE
             subprocess_options['stderr'] = subprocess.STDOUT
         subprocess.run(" ".join(cmd), **subprocess_options)
+
+    def write_inventory(self, list_of_jails: List[Jail], config_folder: PosixPath):
+        inventory: Dict = {
+            'all': {
+                'hosts': {},
+                'vars': {
+                    'ansible_python_interpreter': '/usr/local/bin/python3.6'
+                }
+            }
+        }
+        for jail in list_of_jails:
+            inventory['all']['hosts'][jail.name] = {
+                'hostname': 'localhost',
+                'user': 'root'
+            }
+        with open(config_folder.joinpath(self.ANSIBLE_INVENTORY_NAME).as_posix(), 'w') as inventory_file:
+            yaml.dump(inventory, stream=inventory_file)
+
+    def write_ansible_configuration(self, config_folder: PosixPath):
+        with open(config_folder.joinpath(self.ANSIBLE_CONFIG_FILE).as_posix(), 'w') as ansible_config_file:
+            ansible_config_file.write("[defaults]\n")
+            ansible_config_file.write(f"inventory = {config_folder.joinpath(self.ANSIBLE_INVENTORY_NAME)}\n\n")
+
+            ansible_config_file.write('[ssh_connection]\n')
+            priv_key_path = config_folder.joinpath("priv.key")
+            ansible_config_file.write(
+                f'ssh_args = -C -o ControlMaster=auto -o ControlPersist=60s -i {priv_key_path.as_posix()}\n')
