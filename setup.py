f1cf6b50e333dec09d45034c765b57c788ed5f10
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-07-16 10:37:24 +0200

    Adding a basic setup for the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='jmanager',
+    version='0.1a',
+    description='A python based jail manager',
+    author='Lara Fernandez Cueto',
+    author_email='larafercue@gmail.com',
+    url='https://github.com/LaraFerCue/jmanager',
+    packages=['jmanager', 'jmanager.commands', 'jmanager.factories', 'jmanager/models', 'jmanager.utils']
+)
